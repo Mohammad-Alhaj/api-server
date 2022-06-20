41e6e3f6b05d76804d8c256c9a70755b8d995235
Mohammad Alhaj <mohammadbasil.alhaj@gmail.com>
2022-06-20 23:53:32 +0300

    add UML

--- src/routes/person.js
+++ src/routes/person.js
@@ -12,6 +12,7 @@ personRouter.get("/person/:id",(getUserById))
 personRouter.post("/person",(perosnAlls))
 personRouter.delete("/person/:id",(deleteperson))
 personRouter.put("/person/:id",(updateperson))
+personRouter.get("/test/:name",testIdReading)
 
 async function perosnAll(req, res) {
     let customer = await Person.read();
@@ -35,7 +36,10 @@ async function perosnAlls(req,res){
 async function deleteperson(req,res){
     let id =  parseInt(req.params.id);
     let deleteperson = await Person.delete(id)
-    res.status(204).json(deleteperson)
+    let test = res.status(204)
+    // console.log( test);
+    console.log( {deleteperson});
+
 }
 
 async function updateperson(req, res) {
@@ -48,6 +52,13 @@ async function updateperson(req, res) {
  }else{
     res.status(404).send("can't find the user or id please enter correct id !")
  }
+}
+
+async function testIdReading(req,res){
+    let resding = req.params.name;
+    console.log(resding);
+    let readings = await Person.readByid(resding) ;
+    res.status(200).json(readings)
 }
 
 module.exports = personRouter ;
